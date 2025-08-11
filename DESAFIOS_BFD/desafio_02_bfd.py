cedeb1aba2bcdea3d1e1964f5f111376215c03db
Bartosz-Júnior <edvaldo.sjr94@gmail.com>
2025-08-11 17:59:37 -0300

    Criação do desafio 02

---
+++ DESAFIOS_BFD/desafio_02_bfd.py
@@ -0,0 +1,6 @@
+#CONVERTA A TEMPERATURA DE CELSIUS PARA FAHRENHEIT 
+
+temp_celsius = float(input("Informe a temperatura em ºC: "))
+temp_fahrenheit = (temp_celsius * 1.8) + 32
+
+print(f"A temperatura de {temp_celsius}ºC, equivale a {temp_fahrenheit}F.")
