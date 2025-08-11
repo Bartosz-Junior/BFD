8ce574de3e0a4d44a55d7628174db5ebb2b0d24e
Bartosz-Júnior <edvaldo.sjr94@gmail.com>
2025-08-11 17:53:22 -0300

    Atualização no 1º desafio

--- DESAFIOS_BFD/desafio_01_bfd.py
+++ DESAFIOS_BFD/desafio_01_bfd.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+#CRIE UM PROGRAMA QUE RECEBA OS VALORES DE TRÊS NOTAS DE UM ALUNO E IMPRIMA A MÉDIA.
+
+#ENTRADA DE DADOS(NOTAS)
+nota1 = float(input("Informe a 1ª nota do aluno: "))
+nota2 = float(input("Informe a 2ª nota do aluno: "))
+nota3 =  float(input("Informe a 3ª nota do aluno: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"A média das notas do aluno informada é: {media:.2f}.")
