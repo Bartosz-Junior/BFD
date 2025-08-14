4a23b07289a6d30fca76c513aadb150a10a49989
Bartosz-Júnior <edvaldo.sjr94@gmail.com>
2025-08-14 20:27:05 -0300

    atualização desafio 02

--- DESAFIOS_BFD/LISTAS_TUPLAS_DICIONARIOS/desafio_02_bfd.py
+++ DESAFIOS_BFD/LISTAS_TUPLAS_DICIONARIOS/desafio_02_bfd.py
@@ -19,4 +19,20 @@ while len(lista_geral) != 0:
     del(lista_geral[-1])
     print(lista_geral)
 
+feira_mes = ["Feijão", "Arroz", "Carne", "Detegente", 
+             "Água Sanitária", "Sabão liquido", "Sabão em pó", "Amaciante", "Sorvete"]
 
+feira_mes.remove("Detegente")
+feira_mes.remove("Água Sanitária")
+feira_mes.remove("Sabão liquido")
+feira_mes.remove("Sabão em pó")
+feira_mes.remove("Amaciante")
+feira_mes.remove("Sorvete")
+print(feira_mes)
+
+lista_num = [21 ,32, 13, 45, 65, 67, 57, 84, 29, 10]
+
+lista_num.sort()
+print(lista_num)
+lista_num.reverse()
+print(lista_num)
