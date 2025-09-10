2722d528538881d3d889deaf123dcdbf3622a0b6
Bartosz-Júnior <edvaldo.sjr94@gmail.com>
2025-09-10 13:23:27 -0300

    organização das pastas

--- DESAFIOS_BFD/POO COM PYTHON/Lista_06.py
+++ DESAFIOS_BFD/POO COM PYTHON/Lista_06.py
@@ -312,3 +312,64 @@ minha_biblioteca.listar_livros_disponiveis()
 
 
 #NESTA ULTIMA QUESTÃO EU USEI O GEMINI DO GOOGLE PARA CONSEGUIR RESOLVER O PROBLEMA, NÃO CONSEGUI FAZER SOZINHO
+
+class ContaBancaria:
+    def __init__(self, titular, num_conta, saldo = 0 ): #Método inicializador da classe ContaBancaria
+        self.titular = titular
+        self.num_conta = num_conta
+        self.saldo = saldo
+        self.__extrato = {}
+
+    def extrato(self):
+        print("***** EXTRATO *****")
+        if self.__extrato:
+            for i,v in enumerate(self.__extrato):
+                print(i, v)
+
+    def depositar(self):                        #Depositar, se o saldo em conta for maior que 10 mil reais a cada novo deposito gaha-se um cash back de 5%
+        print("***** DEPOSITAR *****")
+        valor_deposito = float(input("Valor do deposito: R$"))
+        if self.saldo > 10000:
+            self.saldo += valor_deposito + (valor_deposito * 5/100)
+            self.__extrato.update({"Deposito" :valor_deposito })
+            print(f"Você recebeu 5% do valor depositado, por ter pelo menos 10 mil na conta.")
+        else:
+            self.saldo += valor_deposito
+            print("Deposito realizado com sucesso!")
+
+    def sacar(self):            #Método para sacar o saldo em conta
+        print("***** SACAR *****\n")
+        valor_saque = float(input("Valor do saque: "))
+        if self.saldo >= valor_saque:
+            self.saldo -= valor_saque
+            print("Saque realizado com sucesso!\n")
+        else:
+            print("Saldo insuficiente!")
+            print(f"Seu saldo é de R${self.saldo}")
+
+junior = ContaBancaria(str(input("Informe o nome do titular: ")), int(input("Informe a conta: ")))
+
+while True:
+    try:
+        print("____________ MENU ____________")
+        print("[1] - Extrato")
+        print("[2] - Depositar")
+        print("[3] - Sacar")
+        print("[4] - Transferencia entre contas")
+        print("[0] - Sair")
+        print("______________________________")
+
+        opcao = int(input("Escolha uma opção: "))
+
+        match opcao:
+            case 1:
+                junior.extrato()
+            case 2:
+                junior.depositar()
+            case 3:
+                junior.sacar()
+            case 0:
+                break
+
+    except ValueError:
+        print("Opção inválida!")
